7239494f512bb8d874cf8c655129eed6b5020610
Ajyear <ajyeargain2@gmail.com>
2026-03-31 14:35:35 -0500

    Refactored and prepared for control panel implementation

--- main.cpp
+++ main.cpp
@@ -58,6 +58,102 @@ int mandelbrot(double cx, double cy, int maxIterations)
     return iteration;
 }
 
+
+struct ScreenData {
+    static constexpr int WIDTH = 1920, HEIGHT = 1080;
+    int maxIterations = 1000;
+    
+    double aspect = (double)WIDTH / HEIGHT;
+
+    double minReal = -2.5;
+    double maxReal = 1.0;
+    double minImag = -1.5;
+    double maxImag = 1.5;
+
+    bool needsRedraw = true;
+
+    double offsets[4][2] = {
+                {0.25, 0.25},
+                {0.75, 0.25},
+                {0.25, 0.75},
+                {0.75, 0.75}
+    };
+
+
+    double getRealRange() const {
+        return maxReal - minReal;
+    }
+
+    double getImagRange() const {
+        return maxImag - minImag;
+    }
+
+    double getCenterReal() const {
+        return (minReal + maxReal) / 2.0;
+    }
+
+    double getCenterImag() const {
+        return (minImag + maxImag) / 2.0;
+    }
+    void move(double realAmount, double imagAmount) {
+        minReal += realAmount;
+        maxReal += realAmount;
+        minImag += imagAmount;
+        maxImag += imagAmount;
+        needsRedraw = true;
+    }
+    void zoom(double factor) {
+        double realRange = getRealRange();
+        double imagRange = getImagRange();
+
+        double centerReal = getCenterReal();
+        double centerImag = getCenterImag();
+
+        double newRealRange = realRange * factor;
+        double newImagRange = imagRange * factor;
+
+        minReal = centerReal - newRealRange / 2.0;
+        maxReal = centerReal + newRealRange / 2.0;
+        minImag = centerImag - newImagRange / 2.0;
+        maxImag = centerImag + newImagRange / 2.0;
+
+        needsRedraw = true;
+    }
+    void applyBoxZoom(int left, int top, int width, int height) {
+        double realRange = getRealRange();
+        double imagRange = getImagRange();
+
+        double newMinReal = minReal + (double)left / WIDTH * realRange;
+        double newMaxReal = minReal + (double)(left + width) / WIDTH * realRange;
+        double newMinImag = minImag + (double)top / HEIGHT * imagRange;
+        double newMaxImag = minImag + (double)(top + height) / HEIGHT * imagRange;
+
+        minReal = newMinReal;
+        maxReal = newMaxReal;
+        minImag = newMinImag;
+        maxImag = newMaxImag;
+
+        needsRedraw = true;
+    }
+};
+
+struct DragState {
+    bool isDragging = false;
+    sf::Vector2i start;
+    sf::Vector2i end;
+};
+
+struct PanelState {
+    sf::Vector2f position = { 20.f, 20.f };
+    sf::Vector2f size = { 260.f, 180.f };
+
+    bool isDraggingPanel = false;
+    sf::Vector2f dragOffset = { 0.f, 0.f };
+
+    int paletteIndex = 0;
+};
+
+
 void getCorrectedBox(
     sf::Vector2i start,
     sf::Vector2i end,
@@ -86,36 +182,24 @@ void getCorrectedBox(
 }
 
 void renderMandelbrot(sf::Image& image, 
-    int width, 
-    int height, 
-    double minReal, 
-    double maxReal, 
-    double minImag, 
-    double maxImag, 
-    int maxIterations){
-    for (int y = 0; y < height; y++) {
-        for (int x = 0; x < width; x++) {
+   const ScreenData& d){
+    for (int y = 0; y < d.HEIGHT; y++) {
+        for (int x = 0; x < d.WIDTH; x++) {
 
-            double offsets[4][2] = {
-                {0.25, 0.25},
-                {0.75, 0.25},
-                {0.25, 0.75},
-                {0.75, 0.75}
-            };
-
+            
             int totalR = 0;
             int totalG = 0;
             int totalB = 0;
 
             for (int i = 0; i < 4; i++) {
-                double sampleX = x + offsets[i][0];
-                double sampleY = y + offsets[i][1];
+                double sampleX = x + d.offsets[i][0];
+                double sampleY = y + d.offsets[i][1];
 
-                double cx = minReal + sampleX / width * (maxReal - minReal);
-                double cy = minImag + sampleY / height * (maxImag - minImag);
+                double cx = d.minReal + sampleX / d.WIDTH * (d.maxReal - d.minReal);
+                double cy = d.minImag + sampleY / d.HEIGHT * (d.maxImag - d.minImag);
 
-                int iteration = mandelbrot(cx, cy, maxIterations);
-                sf::Color color = getColor(iteration, maxIterations);
+                int iteration = mandelbrot(cx, cy, d.maxIterations);
+                sf::Color color = getColor(iteration, d.maxIterations);
 
                 totalR += color.r;
                 totalG += color.g;
@@ -134,33 +218,19 @@ void renderMandelbrot(sf::Image& image,
     }
 
 int main() {
-    constexpr int WIDTH = 1920, HEIGHT = 1080, MAX_ITERATIONS = 1000;
-
-    double aspect = (double)WIDTH / HEIGHT;
-
-    double minReal = -2.5;
-    double maxReal = 1.0;
-    double minImag = -1.5;
-    double maxImag = 1.5;
-
-    bool needsRedraw = true;
-
-    bool isDragging = false;
-
-    sf::Vector2i dragStart;
-    sf::Vector2i dragEnd;
+    ScreenData d;
+    DragState s;
+    PanelState p;
 
     sf::Image image;
-    image.create(WIDTH, HEIGHT, sf::Color::Black);
+    image.create(d.WIDTH, d.HEIGHT, sf::Color::Black);
 
     sf::Texture texture;
     sf::Sprite sprite(texture);
 
   
 
-   
-
-    sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Mandelbrot Explorer");
+    sf::RenderWindow window(sf::VideoMode(d.WIDTH, d.HEIGHT), "Mandelbrot Explorer");
     window.setFramerateLimit(60);
  
 
@@ -173,98 +243,58 @@ int main() {
             //Key Controls
             if (event.type == sf::Event::KeyPressed) {
 
-                double realRange = maxReal - minReal;
-                double imagRange = maxImag - minImag;
+                double realRange = d.getRealRange();
+                double imagRange = d.getImagRange();
 
-                double moveAmountReal = realRange * 0.1;
-                double moveAmountImag = imagRange * 0.1;
+                double moveAmountReal = d.getRealRange() * 0.1;
+                double moveAmountImag = d.getImagRange() * 0.1;
 
                 if (event.key.code == sf::Keyboard::Left) {
-                    minReal -= moveAmountReal;
-                    maxReal -= moveAmountReal;
-                    needsRedraw = true;
+                    d.move(-moveAmountReal, 0);
                 }
                 else if (event.key.code == sf::Keyboard::Right) {
-                    minReal += moveAmountReal;
-                    maxReal += moveAmountReal;
-                    needsRedraw = true;
+                    d.move(moveAmountReal, 0);
                 }
                 else if (event.key.code == sf::Keyboard::Up) {
-                    minImag -= moveAmountImag;
-                    maxImag -= moveAmountImag;
-                    needsRedraw = true;
+                    d.move(0, -moveAmountImag);
                 }
                 else if (event.key.code == sf::Keyboard::Down) {
-                    minImag += moveAmountImag;
-                    maxImag += moveAmountImag;
-                    needsRedraw = true;
+                    d.move(0, moveAmountImag);
                 }
                 else if (event.key.code == sf::Keyboard::Q) {
-                    double zoomFactor = 0.5;
-                    double centerReal = (minReal + maxReal) / 2.0;
-                    double centerImag = (minImag + maxImag) / 2.0;
-                    double newRealRange = realRange * zoomFactor;
-                    double newImagRange = imagRange * zoomFactor;
-
-                    minReal = centerReal - newRealRange / 2.0;
-                    maxReal = centerReal + newRealRange / 2.0;
-                    minImag = centerImag - newImagRange / 2.0;
-                    maxImag = centerImag + newImagRange / 2.0;
-                    needsRedraw = true;
+                    d.zoom(0.5);
                 }
                 else if (event.key.code == sf::Keyboard::E) {
-                    double zoomFactor = 1.5;
-                    double centerReal = (minReal + maxReal) / 2.0;
-                    double centerImag = (minImag + maxImag) / 2.0;
-                    double newRealRange = realRange * zoomFactor;
-                    double newImagRange = imagRange * zoomFactor;
-
-                    minReal = centerReal - newRealRange / 2.0;
-                    maxReal = centerReal + newRealRange / 2.0;
-                    minImag = centerImag - newImagRange / 2.0;
-                    maxImag = centerImag + newImagRange / 2.0;
-                    needsRedraw = true;
+                    d.zoom(1.5);
                 }
             }
 
             //Mouse Controls
             else if (event.type == sf::Event::MouseButtonPressed) {
-                double realRange = maxReal - minReal;
-                double imagRange = maxImag - minImag;
                 if (event.mouseButton.button == sf::Mouse::Left) {
-                    isDragging = true;
-                    dragStart = sf::Vector2i(event.mouseButton.x, event.mouseButton.y);
-                    dragEnd = dragStart;
+                    s.isDragging = true;
+                    s.start = sf::Vector2i(event.mouseButton.x, event.mouseButton.y);
+                    s.end = s.start;
                 }
             }
             else if (event.type == sf::Event::MouseMoved) {
-                if (isDragging) {
-                    dragEnd = sf::Vector2i(event.mouseMove.x, event.mouseMove.y);
+                if (s.isDragging) {
+                    s.end = sf::Vector2i(event.mouseMove.x, event.mouseMove.y);
                 }
             }
             else if (event.type == sf::Event::MouseButtonReleased) {
-                if (event.mouseButton.button == sf::Mouse::Left && isDragging) {
-                    isDragging = false;
-                    dragEnd = sf::Vector2i(event.mouseButton.x, event.mouseButton.y);
+                if (event.mouseButton.button == sf::Mouse::Left && s.isDragging) {
+                    s.isDragging = false;
+                    s.end = sf::Vector2i(event.mouseButton.x, event.mouseButton.y);
 
-                    double realRange = maxReal - minReal;
-                    double imagRange = maxImag - minImag;
+                    double realRange = d.getRealRange();
+                    double imagRange = d.getImagRange();
                     int top, left, width, height;
 
-                    getCorrectedBox(dragStart, dragEnd, left, top, width, height, (double)WIDTH / HEIGHT);
+                    getCorrectedBox(s.start, s.end, left, top, width, height, (double)d.WIDTH / d.HEIGHT);
 
                     if (width > 5 && height > 5) {
-                        double newMinReal = minReal + (double)left / WIDTH * realRange;
-                        double newMaxReal = minReal + (double)(left + width) / WIDTH * realRange;
-                        double newMinImag = minImag + (double)top / HEIGHT * imagRange;
-                        double newMaxImag = minImag + (double)(top + height) / HEIGHT * imagRange;
-
-                        minReal = newMinReal;
-                        maxReal = newMaxReal;
-                        minImag = newMinImag;
-                        maxImag = newMaxImag;
-
-                        needsRedraw = true;
+                        d.applyBoxZoom(left, top, width, height);
                     }
                 }
             }
@@ -273,22 +303,22 @@ int main() {
             
 
 
-        if (needsRedraw) {
-            renderMandelbrot(image, WIDTH, HEIGHT, minReal, maxReal, minImag, maxImag, MAX_ITERATIONS);
+        if (d.needsRedraw) {
+            renderMandelbrot(image, d);
             texture.loadFromImage(image);
             sprite.setTexture(texture, true);
-            needsRedraw = false;
+            d.needsRedraw = false;
         }
 
 
         window.clear();
         window.draw(sprite);
 
-        if (isDragging) {
+        if (s.isDragging) {
 
             int left, top, width, height;
 
-            getCorrectedBox(dragStart, dragEnd, left, top, width, height, (double)WIDTH / HEIGHT);
+            getCorrectedBox(s.start, s.end, left, top, width, height, d.aspect);
 
             sf::RectangleShape zoomBox;
 
